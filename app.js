02b6f79acb36e50acac99d5d1b619e1eb6e1c36c
larissa.martins <larissa.fiorini@acad.pucrs.br>
2018-07-02 19:34:29 -0300

    Código do server

--- app.js
+++ app.js
@@ -1,63 +1,29 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-var clients = {};
-
-app.get('/', function(req, res){
-  res.redirect('index.html');
+var net = require('net');
+ 
+// Configuration parameters
+var HOST = '192.168.0.134';
+var PORT = 1234;
+ 
+// Create Server instance 
+var server = net.createServer(onClientConnected);  
+ 
+server.listen(PORT, HOST, function() {  
+  console.log('server listening on %j', server.address());
 });
-
-//SocketIO vem aqui
-
-http.listen(3000, function(){
-console.log('listening on port 3000');
-});
-
-io.on("connection", function (client) {
-    console.log('user connected');
-});
-
-io.on("connection", function (client) {
-  client.on("join", function(name){
-    console.log("Joined: " + name);
-    clients[client.id] = name;
-    client.emit("update", "You have connected to the server.");
-    client.broadcast.emit("update", name + " has joined the server.")
+ 
+function onClientConnected(sock) {  
+  var remoteAddress = sock.remoteAddress + ':' + sock.remotePort;
+  console.log('new client connected: %s', remoteAddress);
+ 
+  sock.on('data', function(data) {
+    console.log('%s Says: %s', remoteAddress, data);
+    sock.write(data);
+    sock.write(' exit');
   });
-
-  client.on("send", function(msg){
-    console.log("Message: " + msg);
-    client.broadcast.emit("chat", clients[client.id], msg);
+  sock.on('close',  function () {
+    console.log('connection from %s closed', remoteAddress);
   });
-
-  client.on("disconnect", function(){
-    console.log("Disconnect");
-    io.emit("update", clients[client.id] + " has left the server.");
-    delete clients[client.id];
+  sock.on('error', function (err) {
+    console.log('Connection %s error: %s', remoteAddress, err.message);
   });
-
-  // enviar apenas para o cliente atual
-  client.emit('message', "this is a test");
-
-  // enviar para todos os clientes, inclusive o atual
-  io.emit('message', "this is a test");
-
-  // enviar para todos os clientes, exceto o atual
-  client.broadcast.emit('message', "this is a test");
-
-  // enviar para todos os clientes (com exceção do atual) para uma sala específica
-  socket.broadcast.to('game').emit('message', 'nice game');
-
-  // enviar para todos os clientes em uma sala específica
-  io.in('game').emit('message', 'cool game');
-
-  // enviar para o atual, caso ele esteja na sala
-  client.to('game').emit('message', 'enjoy the game');
-
-  // enviar para todos os clientes em um namespace 'namespace1'
-  io.of('namespace1').emit('message', 'gg');
-
-  // enviando para um socketid individual
-  client.broadcast.to(socketid).emit('message', 'for your eyes only');
-});
+};
