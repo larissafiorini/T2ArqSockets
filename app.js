abba7120e3289b3db01e61d3ce0f88f2e2a3eb76
larissa.martins <larissa.fiorini@acad.pucrs.br>
2018-07-03 19:39:42 -0300

    Recebendo json do cliente

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 var net = require('net');
- 
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+JsonSocket = require('json-socket');
+
+let LançamentoSchema = new Schema({
+  nomeComprador: { type: String, required: true },
+  nomeProduto: { type: String, required: true },
+  valor: { type: Number }
+}, { runSettersOnQuery: true });
+
+let Lançamento = mongoose.model('Lançamento', LançamentoSchema);
 // Configuration parameters
 var HOST = '192.168.0.134';
 var PORT = 1234;
- 
+
 // Create Server instance 
-var server = net.createServer(onClientConnected);  
- 
-server.listen(PORT, HOST, function() {  
+var server = net.createServer(onClientConnected);
+
+
+var MongoClient = require('mongodb').MongoClient;
+//Create a database named "mydb":
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Connected");
+
+  db.close();
+});
+
+server.listen(PORT, HOST, function () {
   console.log('server listening on %j', server.address());
 });
- 
+
+
+
+
+
 function onClientConnected(sock) {  
   var remoteAddress = sock.remoteAddress + ':' + sock.remotePort;
   console.log('new client connected: %s', remoteAddress);
@@ -19,6 +47,9 @@ function onClientConnected(sock) {
     console.log('%s Says: %s', remoteAddress, data);
     sock.write(data);
     sock.write(' exit');
+    // socket json
+    socket = new JsonSocket(sock);
+    console.log(socket);
   });
   sock.on('close',  function () {
     console.log('connection from %s closed', remoteAddress);
